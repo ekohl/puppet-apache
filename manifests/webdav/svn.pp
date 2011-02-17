145818930664fcd9aa7dccbce1166476b2a9f99a
Atha Kouroussis <akouroussis@gmail.com>
2011-02-17 17:59:04 -0300

    Starting refactor
    - Directory structure and file naming to eliminate imports
    - One file per class
    - Code style

---
+++ manifests/webdav/svn.pp
@@ -0,0 +1,16 @@
+define apache::webdav::svn ($ensure=present, $vhost, $parentPath, $confname) {
+	include apache::params
+
+	$location = $name
+
+	file { "${apache::params::root}/${vhost}/conf/${confname}.conf":
+		ensure  => $ensure,
+		content => template("apache/webdav-svn.erb"),
+		seltype => $operatingsystem ? {
+			"RedHat" => "httpd_config_t",
+			"CentOS" => "httpd_config_t",
+			default  => undef
+		},
+		notify  => Exec["apache-graceful"]
+	}
+}
