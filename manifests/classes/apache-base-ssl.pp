3711ab9d0423cb5715d1c5aa55bfcff615d791ce
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-09-15 09:46:46 +0200

    Remove the option to generate CSRs

--- manifests/classes/apache-base-ssl.pp
+++ manifests/classes/apache-base-ssl.pp
@@ -11,10 +11,4 @@ class apache::base::ssl {
 
   apache::listen { "443": ensure => present }
   apache::namevhost { "*:443": ensure => present }
-
-  file { "/usr/local/sbin/generate-ssl-cert.sh":
-    source => "puppet:///modules/apache/generate-ssl-cert.sh",
-    mode   => 755,
-  }
-
 }
